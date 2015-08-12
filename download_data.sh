14a4b36ed2050b55a927cd14199c8072276a323d
Mike Gevaert <michael.gevaert@epfl.ch>
2015-08-12 12:17:32 +0200

    fixed typo in download of 1.json
    Change-Id: Ib83d43283fe16b96e4bfad73f8e35e2b3aa20b99

--- download_data.sh
+++ download_data.sh
@@ -4,11 +4,10 @@ mkdir data
 pushd data
 wget -O atlasVolume.zip http://api.brain-map.org/api/v2/well_known_file_download/113567585
 wget -O P56_Mouse_annotation.zip http://api.brain-map.org/api/v2/well_known_file_download/197642854
-wget http://api.brain-map.org/api/v2/structure_graph_download/1.jso
+wget http://api.brain-map.org/api/v2/structure_graph_download/1.json
 unzip atlasVolume.zip
 mkdir P56_Mouse_annotation
 pushd P56_Mouse_annotation
 unzip ../P56_Mouse_annotation.zip
 popd
 mv 1.json P56_Mouse_annotation/annotation_hierarchy.json
-
