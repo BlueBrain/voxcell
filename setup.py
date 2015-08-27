497af78c6a0db2a6f09c510d84447171e95684ac
courcol <jean-denis.courcol@epfl.ch>
2015-08-27 09:21:42 +0200

    added argparse as requirements and utils as subpackage
    Change-Id: Iec5417e68b0ddc887f11d5bdf3646506dd72cb70

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     version=__version__,
     description='code to build brains',
     install_requires=REQS,
-    packages=['brainbuilder'],
+    packages=['brainbuilder', 'brainbuilder.utils'],
     include_package_data=True,
     scripts=[],
 )
