5d62828452dcbfacc89bfa12476d9457f61727d5
Mike Gevaert <michael.gevaert@epfl.ch>
2015-09-01 11:26:25 +0200

    added serializer for synapse class format
    Change-Id: I5c55651be91c6fbd15e6605e9a1580474a39be43

--- brainbuilder/assignment_synapse_class.py
+++ brainbuilder/assignment_synapse_class.py
@@ -32,3 +32,37 @@ def assign_synapse_class_from_spatial_dist(positions, spatial_dist, voxel_dimens
     '''
     chosen_sclass = tt.assign_from_spatial_distribution(positions, spatial_dist, voxel_dimensions)
     return spatial_dist.traits['sClass'][chosen_sclass].as_matrix()
+
+
+def serialize_assign_synapse_class(dst_file, assigned_sclass):
+    '''Serialize assigned synapse classes
+
+    Args:
+        dst_file(str): fullpath to filename to write
+        assigned_sclass: list/np.array of strings which are sclass names
+            EX: ('inhibitory', 'excitatory')
+    '''
+    with open(dst_file, 'w') as fd:
+        for sclass in assigned_sclass:
+            fd.write('%s\n' % sclass)
+
+
+def deserialize_assign_synapse_class(src_file):
+    '''De-serialize assigned synapse class
+
+    Args:
+        src_file(str): fullpath to filename to write
+
+    Returns:
+        np.array of strings which are sclass names
+    '''
+    sclass = []
+    with open(src_file, 'r') as fd:
+        for line in fd.readlines():
+            line = line.strip()
+            if line == 'nan':
+                sclass.append(np.nan)
+            else:
+                sclass.append(line)
+
+    return np.array(sclass)
--- brainbuilder/tests/test_assignment_synapse_class.py
+++ brainbuilder/tests/test_assignment_synapse_class.py
@@ -1,22 +1,34 @@
+import tempfile
+
 from nose.tools import eq_
 import numpy as np
 from numpy.testing import assert_equal
 
-from brainbuilder.assignment_synapse_class import assign_synapse_class_randomly
+import brainbuilder.assignment_synapse_class as ASC
 
 
-def test_assign_sypnapse_class_empty():
-    sclass = assign_synapse_class_randomly(np.zeros(shape=(0, 3)), inhibitory_fraction=0)
+def test_assign_synapse_class_empty():
+    sclass = ASC.assign_synapse_class_randomly(np.zeros(shape=(0, 3)), inhibitory_fraction=0)
     eq_(sclass.shape, (0,))
 
 
-def test_assign_sypnapse_class_all_excitatory():
-    result = assign_synapse_class_randomly(np.zeros(shape=(10, 3)), inhibitory_fraction=0)
+def test_assign_synapse_class_all_excitatory():
+    result = ASC.assign_synapse_class_randomly(np.zeros(shape=(10, 3)), inhibitory_fraction=0)
     eq_(result.shape, (10,))
     assert_equal(result, np.array(['excitatory'] * 10))
 
 
-def test_assign_sypnapse_class_all_inhibitory():
-    result = assign_synapse_class_randomly(np.zeros(shape=(10, 3)), inhibitory_fraction=1)
+def test_assign_synapse_class_all_inhibitory():
+    result = ASC.assign_synapse_class_randomly(np.zeros(shape=(10, 3)), inhibitory_fraction=1)
     eq_(result.shape, (10,))
     assert_equal(result, np.array(['inhibitory'] * 10))
+
+
+def test_serialization():
+    sclasses = ASC.assign_synapse_class_randomly(np.zeros(shape=(10, 3)), inhibitory_fraction=1)
+    sclasses[0] = np.nan
+
+    with tempfile.NamedTemporaryFile() as f:
+        ASC.serialize_assign_synapse_class(f.name, sclasses)
+        new_sclasses = ASC.deserialize_assign_synapse_class(f.name)
+        assert_equal(sclasses, new_sclasses)
