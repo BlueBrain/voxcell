5ed164293ead7d0d5e78c7151e5fd82656d558a6
jean-denis courcol <jean-denis.courcol@epfl.ch>
2015-12-15 16:32:00 +0100

    support pip > 1.5.x
    Change-Id: I6b404f01016f3b0678b0f3c0890c8bea33937261

--- voxcellview/setup.py
+++ voxcellview/setup.py
@@ -8,16 +8,26 @@ try:
 except ImportError:
     from distutils.core import setup
 
-
+import pip
 from pip.req import parse_requirements
 from optparse import Option
 
 from voxcellview import __version__
+options = Option('--workaround')
+options.skip_requirements_regex = None
+REQ_FILE = './requirements.txt'
+# Hack for old pip versions: Versions greater than 1.x
+# have a required parameter "sessions" in parse_requierements
+if pip.__version__.startswith('1.'):
+    install_reqs = parse_requirements(REQ_FILE, options=options)
+else:
+    from pip.download import PipSession  # pylint:disable=E0611
+    options.isolated_mode = False
+    install_reqs = parse_requirements(REQ_FILE,  # pylint:disable=E1123
+                                      options=options,
+                                      session=PipSession)
 
-OPTIONS = Option("--workaround")
-OPTIONS.skip_requirements_regex = None
-INSTALL_REQS = parse_requirements("./requirements.txt", options=OPTIONS)
-REQS = [str(ir.req) for ir in INSTALL_REQS]
+REQS = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='voxcellview',
