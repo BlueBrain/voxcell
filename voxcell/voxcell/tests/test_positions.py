cdc13c25f6aec3a7cbe69deba94079fe63bb5983
Luis Riquelme <luis.riquelme@epfl.ch>
2015-12-17 08:37:49 +0100

    moved cell positioning code to voxcell
    this makes voxcell more selfcontained, as different classes in it
    were always talking about "positions" but we weren't providing
    even a basic way to create these.
    
    kept the module in brainbuilder as a thin wrapper, in the same
    fashion as all the assign_* modules, so that future development
    happens there.
    
    once more advanced positioning develops into brainbuilder
    (with cell avoidance for example), if we find that the algorithms
    become generic (not workflow/bbp/region-specific), they can also
    be moved into voxcell.
    
    Change-Id: I39afd487101b42b5392edf1e5b034262e71a1707

--- voxcell/voxcell/tests/test_positions.py
+++ voxcell/voxcell/tests/test_positions.py
@@ -1,19 +1,18 @@
-from nose.tools import eq_
-
 import numpy as np
-from brainbuilder import cell_positioning
-from voxcell import core
+from nose.tools import eq_
 from numpy.testing import assert_equal
-from brainbuilder import cell_positioning as cp
+
+from voxcell import positions
+from voxcell import core
 
 
 def test_cell_counts_to_cell_voxel_indices_0():
-    assert_equal(cp.cell_counts_to_cell_voxel_indices(np.zeros((3, 3, 3), dtype=np.int32)),
+    assert_equal(positions._cell_counts_to_cell_voxel_indices(np.zeros((3, 3, 3), dtype=np.int32)),
                  np.empty(shape=(0, 3), dtype=np.int64))
 
 
 def test_cell_counts_to_cell_voxel_indices_1():
-    assert_equal(cp.cell_counts_to_cell_voxel_indices(np.ones((3, 3, 3), dtype=np.int32)),
+    assert_equal(positions._cell_counts_to_cell_voxel_indices(np.ones((3, 3, 3), dtype=np.int32)),
                  np.array([[0, 0, 0],
                           [0, 0, 1],
                           [0, 0, 2],
@@ -48,7 +47,7 @@ def test_cell_counts_to_cell_voxel_indices_complex():
     original[0, 0, 0] = 2
     original[1, 1, 1] = 1
     original[2, 2, 2] = 3
-    assert_equal(cp.cell_counts_to_cell_voxel_indices(original),
+    assert_equal(positions._cell_counts_to_cell_voxel_indices(original),
                  np.array([[0, 0, 0],
                            [0, 0, 0],
                            [1, 1, 1],
@@ -57,31 +56,31 @@ def test_cell_counts_to_cell_voxel_indices_complex():
                            [2, 2, 2]], dtype=np.int64))
 
 
-def test_assign_cell_counts_homogeneous_0():
+def test_create_cell_counts_homogeneous_0():
     cell_body_density_raw = np.ones((3, 3, 3), dtype=np.int)
     total_cell_count = 0
 
-    result = cp.assign_cell_counts(cell_body_density_raw, total_cell_count)
+    result = positions.create_cell_counts(cell_body_density_raw, total_cell_count)
     eq_(np.shape(result), np.shape(cell_body_density_raw))
     eq_(np.sum(result), total_cell_count)
 
     assert_equal(result, np.zeros(np.shape(result)))
 
 
-def test_assign_cell_counts_homogeneous_1():
+def test_create_cell_counts_homogeneous_1():
     cell_body_density_raw = np.ones((3, 3, 3), dtype=np.int)
     total_cell_count = 1
 
-    result = cp.assign_cell_counts(cell_body_density_raw, total_cell_count)
+    result = positions.create_cell_counts(cell_body_density_raw, total_cell_count)
     eq_(np.shape(result), np.shape(cell_body_density_raw))
     eq_(np.sum(result), total_cell_count)
 
 
-def test_assign_cell_counts_homogeneous_full():
+def test_create_cell_counts_homogeneous_full():
     cell_body_density_raw = np.ones((3, 3, 3), dtype=np.int)
     total_cell_count = (3 * 3 * 3)
 
-    result = cp.assign_cell_counts(cell_body_density_raw, total_cell_count)
+    result = positions.create_cell_counts(cell_body_density_raw, total_cell_count)
     eq_(np.shape(result), np.shape(cell_body_density_raw))
     eq_(np.sum(result), total_cell_count)
 #    assert_equal(result, np.ones(np.shape(result)))
@@ -90,7 +89,7 @@ def test_assign_cell_counts_homogeneous_full():
 def test_cell_voxel_indices_to_positions_0():
     cell_voxel_indices = np.zeros((0, 3), dtype=np.int)
     voxel_dimensions = (3, 3, 3)
-    result = cell_positioning._cell_voxel_indices_to_positions(cell_voxel_indices, voxel_dimensions)
+    result = positions._cell_voxel_indices_to_positions(cell_voxel_indices, voxel_dimensions)
 
     eq_(np.shape(result), (0, 3))
 
@@ -98,7 +97,7 @@ def test_cell_voxel_indices_to_positions_0():
 def test_cell_voxel_indices_to_positions_1():
     cell_voxel_indices = np.zeros((1, 3), dtype=np.int)
     voxel_dimensions = (0, 1, 100)
-    result = cell_positioning._cell_voxel_indices_to_positions(cell_voxel_indices, voxel_dimensions)
+    result = positions._cell_voxel_indices_to_positions(cell_voxel_indices, voxel_dimensions)
 
     eq_(np.shape(result), (1, 3))
     eq_(result[0, 0], 0)
@@ -110,7 +109,7 @@ def test_cell_voxel_indices_to_positions_2():
     cell_voxel_indices = np.zeros((2, 3), dtype=np.int)
     cell_voxel_indices[1] = np.ones((1, 3))
     voxel_dimensions = (0, 1, 100)
-    result = cell_positioning._cell_voxel_indices_to_positions(cell_voxel_indices, voxel_dimensions)
+    result = positions._cell_voxel_indices_to_positions(cell_voxel_indices, voxel_dimensions)
 
     eq_(np.shape(result), (2, 3))
 
@@ -126,31 +125,31 @@ def test_cell_voxel_indices_to_positions_2():
 mhd = {'voxel_dimensions': (25, 25, 25)}
 
 
-def test_cell_positioning_0():
+def test_create_cell_positions_0():
     raw = np.ones((3, 3, 3))
     total_cell_count = 0
 
-    result = cp.cell_positioning(core.VoxelData(raw, **mhd), total_cell_count)
+    result = positions.create_cell_positions(core.VoxelData(raw, **mhd), total_cell_count)
 
     eq_(np.shape(result), (total_cell_count, 3))
     assert np.all((result >= 0) & (result <= 3 * 25))
 
 
-def test_cell_positioning_1():
+def test_create_cell_positions_1():
     raw = np.ones((3, 3, 3))
     total_cell_count = 1
 
-    result = cp.cell_positioning(core.VoxelData(raw, **mhd), total_cell_count)
+    result = positions.create_cell_positions(core.VoxelData(raw, **mhd), total_cell_count)
 
     eq_(np.shape(result), (total_cell_count, 3))
     assert np.all((result >= 0) & (result <= 3 * 25))
 
 
-def test_cell_positioning_full():
+def test_create_cell_positions_full():
     raw = np.ones((3, 3, 3))
     total_cell_count = 3 * 3 * 3
 
-    result = cp.cell_positioning(core.VoxelData(raw, **mhd), total_cell_count)
+    result = positions.create_cell_positions(core.VoxelData(raw, **mhd), total_cell_count)
 
     eq_(np.shape(result), (total_cell_count, 3))
     assert np.all((result >= 0) & (result <= 3 * 25))
