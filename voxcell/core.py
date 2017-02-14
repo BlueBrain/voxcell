4444c2fba1905f3134468b94dcb206b94ce8d4d6
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2017-02-14 12:00:10 +0100

    Introduced RegionMap class
    Change-Id: I72d9e28d142f8de791a5d36df2d9d138eaa29b04

--- voxcell/core.py
+++ voxcell/core.py
@@ -1,6 +1,7 @@
 '''core container classes'''
 
 import itertools
+import copy
 import json
 import logging
 
@@ -189,6 +190,72 @@ class Hierarchy(object):
             txt += '\n    children: []'
 
         return txt
+
+
+class RegionMap(object):
+    """ Encapsulates brain region hierarchy retrieved from NIP. """
+    def __init__(self):
+        self._data = dict()
+        self._children = dict()
+        self._parent = dict()
+
+    def get(self, _id):
+        """ Get the payload data associated with region ID. """
+        return self._data[_id]
+
+    @classmethod
+    def from_dict(cls, d):
+        """ Construct RegionMap from a hierarchical dictionary. """
+        def include(data, parent_id):
+            # pylint: disable=protected-access,missing-docstring
+            _id = data.pop('id')
+            if _id in result._data:
+                raise VoxcellError("Duplicate id: %d" % _id)
+            children = data.pop('children', [])
+            result._data[_id] = data
+            result._parent[_id] = parent_id
+            result._children[_id] = [c['id'] for c in children]
+            for c in children:
+                include(c, _id)
+        result = cls()
+        include(copy.deepcopy(d), None)
+        return result
+
+    @classmethod
+    def from_json(cls, filepath):
+        """ Construct RegionMap from JSON file. """
+        with open(filepath, 'r') as f:
+            return cls.from_dict(json.load(f))
+
+    def ids(self, value, attr='name', with_descendants=True):
+        """ Get set of regions matching the given attribute. """
+        result = set()
+        for _id, data in self._data.iteritems():
+            if data.get(attr) == value:
+                if with_descendants:
+                    result.update(self.descendants(_id))
+                else:
+                    result.add(_id)
+        return result
+
+    def ascendants(self, _id):
+        """ Get a list of ascendants for the given id (itself included).
+
+            The list is sorted in level descending order.
+        """
+        x = _id
+        result = []
+        while x is not None:
+            result.append(x)
+            x = self._parent[x]
+        return result
+
+    def descendants(self, _id):
+        """ Get a set of descendants for the given id (itself included). """
+        result = set([_id])
+        for c in self._children[_id]:
+            result.update(self.descendants(c))
+        return result
 
 
 class CellCollection(object):
---
+++ voxcell/tests/test_region_map.py
@@ -0,0 +1,85 @@
+import os
+from nose.tools import eq_, raises
+
+from voxcell import VoxcellError
+import voxcell.core as test_module
+
+
+DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
+
+TEST_RMAP = {
+    'id': 1,
+    'name': 'A',
+    'children': [
+        {
+            'id': 2,
+            'name': 'B',
+        },
+        {
+            'id': 3,
+            'name': 'C',
+            'children': [
+                {
+                    'id': 4,
+                    'name': 'B',
+                }
+            ]
+        }
+    ]
+}
+
+
+def test_from_json():
+    filepath = os.path.join(DATA_PATH, 'region_map.json')
+    rmap = test_module.RegionMap.from_json(filepath)
+    eq_(rmap.get(567), {
+        "atlas_id": 70,
+        "ontology_id": 1,
+        "acronym": "CH",
+        "name": "Cerebrum",
+        "color_hex_triplet": "B0F0FF",
+        "graph_order": 2,
+        "st_level": None,
+        "hemisphere_id": 3,
+        "parent_structure_id": 8
+    })
+
+
+@raises(VoxcellError)
+def test_duplicate_id():
+    test_module.RegionMap.from_dict({
+        'id': 1,
+        'children': [
+            {
+                'id': 2
+            },
+            {
+                'id': 1
+            }
+        ]
+    })
+
+
+def test_get():
+    rmap = test_module.RegionMap.from_dict(TEST_RMAP)
+    eq_(rmap.get(1), {'name': 'A'})
+    eq_(rmap.get(4), {'name': 'B'})
+
+
+def test_ascendants():
+    rmap = test_module.RegionMap.from_dict(TEST_RMAP)
+    eq_(rmap.ascendants(1), [1])
+    eq_(rmap.ascendants(3), [3, 1])
+
+
+def test_descendants():
+    rmap = test_module.RegionMap.from_dict(TEST_RMAP)
+    eq_(rmap.descendants(1), set([1, 2, 3, 4]))
+    eq_(rmap.descendants(3), set([3, 4]))
+
+
+def test_ids():
+    rmap = test_module.RegionMap.from_dict(TEST_RMAP)
+    eq_(rmap.ids('B'), set([2, 4]))
+    eq_(rmap.ids('C'), set([3, 4]))
+    eq_(rmap.ids('C', with_descendants=False), set([3]))
