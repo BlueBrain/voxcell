d2cf9b3997bb071503551d2cb3e9962fcdd2d8f8
Liesbeth Vanherpe <liesbeth.vanherpe@epfl.ch>
2017-10-19 16:38:15 +0200

    Reproducible unit tests for poisson disc sampling
    Change-Id: I6a9f26f3143bf850fe0e204eb7259cf252f04a9c

--- brainbuilder/brainbuilder/tests/test_poisson_disc_sampling.py
+++ brainbuilder/brainbuilder/tests/test_poisson_disc_sampling.py
@@ -7,8 +7,17 @@ from voxcell import VoxelData
 import brainbuilder.poisson_disc_sampling as test_module
 
 
+def setup_func():
+    np.random.seed(42)
+
+
+def teardown_func():
+    pass
+
+
+@nt.with_setup(setup_func, teardown_func)
 def test_generate_points():
-    domain = np.array([[0, 0, 0], [100, 200, 50]])
+    domain = np.array([[0, 0, 0], [100, 200, 500]])
     nb_points = 20
     min_distance = 5
     seed = np.array([0, 0, 0])
@@ -26,6 +35,7 @@ def test_generate_points():
         nt.assert_true(np.all(point >= domain[0,:]) and np.all(point <= domain[1,:]))
 
 
+@nt.with_setup(setup_func, teardown_func)
 def test_generate_points_too_many():
     domain = np.array([[0, 0, 0], [10, 20, 5]])
     nb_points = 1000
@@ -44,6 +54,7 @@ def test_generate_points_too_many():
         nt.assert_true(np.all(point >= domain[0,:]) and np.all(point <= domain[1,:]))
 
 
+@nt.with_setup(setup_func, teardown_func)
 def test_generate_points_random_seed():
     domain = np.array([[0, 0, 0], [100, 200, 50]])
     nb_points = 20
@@ -60,6 +71,7 @@ def test_generate_points_random_seed():
         nt.assert_true(np.all(point >= domain[0,:]) and np.all(point <= domain[1,:]))
 
 
+@nt.with_setup(setup_func, teardown_func)
 def test_generate_points_random_seed_neg_domain():
     domain = np.array([[-50, -100, -25], [50, 100, 25]])
     nb_points = 20
