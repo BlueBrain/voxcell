47cbb9b4ddeb4efb2daae208ac6a42df6d84ab3e
lguyot <luc.guyot@epfl.ch>
2020-10-07 16:19:28 +0200

    [NSETM-1170] A function to retrieve the region ids of layers in a brain region
    This change extends the class LocalAtlas with a method that retrieves
    the region identifers (e.g., the AIBS StructureIDs) of every layer specified in
    a companion metadata file named metadata.json.
    
    The function get_layer_volume lets you retrieve the volume where each voxel
    is labeled with index of the layer it belongs to.
    
    Currently, the use cases are the following:
    * Retrieve the AIBS region identifiers of each layer in the mouse isocortex
    * Retrieve the Paxinos-Watson region identifiers of each layer in the rat S1 brain region
    
    Usage:
      from voxcell.nexus.voxelbrain import LocalAtlas
      import numpy.testing as npt
      atlas = LocalAtlas('atlases/aibs_isocortex_25_2017.nrrd')
      names, ids = atlas.get_layer_ids()
      layer_volume = atlas.get_layer_volume() # Returns a VoxelData object.
      npt.assert_array_equal(layer_volume.raw > 0, atlas.load_data('brain_regions.nrrd').raw > 0)
    
    Change-Id: Iadd5640ae9f42cf71d26ebf5d412d948011372fe

--- tests/test_voxelbrain.py
+++ tests/test_voxelbrain.py
@@ -1,8 +1,10 @@
-import os
+from pathlib import Path
 import json
 import nose.tools as nt
 import numpy as np
 import numpy.testing as npt
+
+DATA_PATH = Path(Path(__file__).parent, 'data')
 
 from mock import patch
 
@@ -22,6 +24,9 @@ def test_open_atlas():
 class TestLocalAtlas(object):
     def setUp(self):
         self.atlas = test_module.Atlas.open('/foo')
+        with open(str(Path(DATA_PATH, 'metadata.json')), 'r') as metadata_file:
+            self.metadata = json.load(metadata_file)
+        self.hierarchy = RegionMap.load_json(str(Path(DATA_PATH, 'hierarchy.json')))
 
     def test_class(self):
         nt.assert_is_instance(self.atlas, test_module.LocalAtlas)
@@ -39,6 +44,15 @@ class TestLocalAtlas(object):
         actual = self.atlas.load_region_map()
         nt.assert_equal(actual, 'test')
         mock.assert_called_with('/foo/hierarchy.json')
+
+    @patch('os.path.exists', return_value=True)
+    @patch('voxcell.nexus.voxelbrain.open')
+    def test_load_metadata(self, open_mock, _):
+        with open(str(Path(DATA_PATH, 'metadata.json')), 'r') as metadata_file:
+            open_mock.return_value = metadata_file
+            metadata = self.atlas.load_metadata()
+            if 'layers' in metadata:
+                assert all(key in metadata['layers'] for key in ['names', 'queries', 'attribute'])
 
     @patch('os.path.exists', return_value=True)
     @patch('voxcell.VoxelData.load_nrrd', return_value='test')
@@ -103,3 +117,58 @@ class TestLocalAtlas(object):
             VoxcellError,
             self.atlas.get_region_mask, 'aa'
         )
+
+
+class TestLocalAtlas(object):
+    def setUp(self):
+        self.atlas = test_module.Atlas.open(str(DATA_PATH))
+        expected_data = (('layer 1', set([1140, 1125])),
+                         ('layer 2', set([1141, 1126])),
+                         ('layer 3', set([517, 1142, 1127])),
+                         ('layer 4', set([1128])),
+                         ('layer 5', set([1129])),
+                         ('layer 6', set([1130])),
+                         ('L1', set([1125])),
+                         )
+        self.expected_names = [v[0] for v in expected_data]
+        self.expected_ids = [v[1] for v in expected_data]
+
+    def test_get_layer(self):
+        for i in range(7):
+            layer, ids = self.atlas.get_layer(i)
+            assert self.expected_names[i]== layer
+            assert self.expected_ids[i] == ids
+
+    @patch('voxcell.nexus.voxelbrain.LocalAtlas.load_region_map', return_value={})
+    @patch('voxcell.nexus.voxelbrain.LocalAtlas.load_metadata')
+    def test_get_layer_ids_raise(self, load_metadata_mock, _):
+        load_metadata_mock.return_value = {'layers': {'name': ['layer1', 'layer4']}}
+        with nt.assert_raises(VoxcellError):
+            self.atlas.get_layer(0)
+
+        load_metadata_mock.return_value = \
+            {'layers': {'names': ['layer1', 'layer4'], 'queries': ['@.*1$', '@.*4$']}}
+        with nt.assert_raises(VoxcellError):
+            self.atlas.get_layer(1)
+
+        load_metadata_mock.return_value = \
+            {'layers': {'names': ['layer1', 'layer4'], 'queries': ['@.*4$'], 'attribute': 'name'}}
+        with nt.assert_raises(VoxcellError):
+            self.atlas.get_layer(2)
+
+
+    def test_get_layer_ids(self):
+        names, ids = self.atlas.get_layers()
+        npt.assert_equal(names, self.expected_names)
+        for id_, expected in zip(ids,  self.expected_ids):
+            assert id_ == expected
+
+    @patch('voxcell.nexus.voxelbrain.LocalAtlas.load_data')
+    def test_get_layer_volume(self, load_data_mock):
+        load_data_mock.return_value = VoxelData(
+            np.array([[[1125], [1126], [1130]]]), voxel_dimensions=(1.0, 1.0, 1.0))
+
+        layer_volume = self.atlas.get_layer_volume()
+        # In metadata.json, 'L1' and '@.*1' defines the same set of ids.
+        # L1 wins because it comes at the end.
+        npt.assert_array_equal(layer_volume.raw, np.array([[[7], [2], [6]]]))
--- voxcell/nexus/voxelbrain.py
+++ voxcell/nexus/voxelbrain.py
@@ -6,6 +6,7 @@ https://nip.humanbrainproject.eu/documentation/user-manual.html#voxel-brain
 
 import os
 import abc
+import json
 import requests
 
 try:
@@ -14,6 +15,7 @@ except ImportError:
     # pylint: disable=no-name-in-module,import-error
     from urllib import parse as urlparse
 
+import numpy as np
 
 from voxcell import VoxelData, Hierarchy, RegionMap
 from voxcell import math_utils
@@ -199,3 +201,167 @@ class LocalAtlas(Atlas):
     def fetch_hierarchy(self):
         """ Return filepath to brain region hierarchy JSON. """
         return self._get_filepath("hierarchy.json")
+
+    def fetch_metadata(self):
+        """ Return filepath to metadata JSON. """
+        return self._get_filepath("metadata.json")
+
+    def load_metadata(self, memcache=False):
+        """ Load brain region metadata as dict. """
+
+        def _callback():
+            with open(self.fetch_metadata(), 'r') as f:
+                return json.load(f)
+
+        return self._check_cache(('metadata',), callback=_callback, memcache=memcache)
+
+    def get_layers(self):
+        """
+        Retrieve and cache the identifiers of each layer of a laminar brain region.
+
+        For each layer of an annotated brain volume, the function returns the set of identifiers of
+        all regions included in this layer.
+
+        Note: this function relies on the existence of the files
+            * hierarchy.json
+            * metadata.json
+
+        See get_layer for the description of the metadata.json file.
+
+        Returns: tuple (names, ids) where `names` is a list of layer names where `ids` is a list
+            of sets and. Each set contains the identifiers (ints) of the corresponding layer name
+            (str).
+
+        Raises: VoxcellError
+            * if the hierarchy file or the metadata file doesn't exist.
+            * if metadata.json doesn't contain the key "layers"
+            * if the value of "layers" doesn't contain all the required keys:
+                "names", "queries" and "attribute".
+            * if the value  of "names" or "ids" is not a list, or if these objects are two lists
+            of different lengths.
+        """
+
+        def _callback():
+            metadata = self.load_metadata()
+
+            if 'layers' not in metadata:
+                raise VoxcellError('Missing "layers" key')
+            layers = metadata['layers']
+
+            if not all(
+                key in metadata['layers'] for key in ['names', 'queries', 'attribute']
+            ):
+                err_msg = (
+                    'Missing some "layers" key. The "layers" dictionary has '
+                    'the following mandatory keys: "names", "queries" and "attribute"'
+                )
+                raise VoxcellError(err_msg)
+
+            if not (
+                isinstance(layers['names'], list)
+                and isinstance(layers['queries'], list)
+                and len(layers['names']) == len(layers['queries'])
+            ):
+                raise VoxcellError(
+                    'The values of "names" and "queries" must be lists of the same length'
+                )
+
+            region_map = self.load_region_map()
+
+            ids = [
+                region_map.find(query, attr=layers['attribute'], with_descendants=True)
+                for query in layers['queries']
+            ]
+
+            return (layers['names'], ids)
+
+        return self._check_cache(
+            ('get_layer_ids'),
+            callback=_callback,
+            memcache=True,
+        )
+
+    def get_layer(self, layer_index):
+        """
+        Retrieve the identifiers of a specified layer in laminar brain region.
+
+        Given a layer of an annotated brain volume, the function returns the set of identifiers of
+        all regions included in this layer.
+
+        Note: this function relies on the existence of the files
+            * hierarchy.json
+            * metadata.json
+
+        The content of metadata.json must be of the following form:
+        {
+            ...
+            "layers": {
+                "names": [
+                    "layer 1", "layer 2/3", "layer 4", "layer 5", "layer 6", "Olfactory areas"
+                ],
+                "queries": ["@.*1$", "@.*2/3$", "@.*4$", "@.*5$", ".*6[a|b]?$", "OLF"],
+                "attribute": "acronym"
+            },
+            ...
+        }
+        The strings of the `queries` list are in one-to-one correspondence with layer `names`.
+        The layer `names` list is a user-defined string list which can vary depending on which
+        hierarchy.json is used and which brain region is under scrutiny.
+        Each query string is used to retrieve the region identifiers of the corresponding
+        layer by means of the Atlas RegionMap object instantiated from the hierarchy.json file.
+        The syntax of a query string is the same as the one in use for RegionMap.find:
+        if the string starts with the symbol '@', the remainder is interpreted as a regular
+        expression. Otherwise it is a plain string value and RegionMap.find looks for a
+        character-wise full match.
+        The value of `attribute` is `acronym` or `name`. This unique value applies for every query
+        string.
+
+        Returns: tuple (name, ids) where `name` is the name of the layer (str) with index
+            `layer_index` and `ids` is the set of region identifiers of every region
+            in this layer according to hierarchy.json.
+
+        Raises: VoxcellError
+            * if the hierarchy file or the metadata file doesn't exist.
+            * if metadata.json doesn't contain the key "layers"
+            * if the value of "layers" doesn't contain all the required keys:
+                "names", "queries" and "attribute".
+            * if the value  of "names" or "ids" is not a list, or if these objects are two lists
+            of different lengths.
+        """
+        layer_names, layer_ids = self.get_layers()
+
+        return (layer_names[layer_index], layer_ids[layer_index])
+
+    def get_layer_volume(self, memcache=False):
+        """VoxelData whose voxels are labeled with the layer indices of `brain_regions` voxels.
+
+        Layer indices range from 1 to `number of layers` - 1. The layers of the atlas are defined
+        within metadata.json as a list of RegionMap.find query strings.
+
+        If two layer definitions in metadata.json involve the same region identifier,
+        the voxels bearing this identifier will be labled with the largest layer index.
+
+        Args:
+            memcache: If True, use cache, Otherwise re-compute the layer volume.
+                Defaults to False.
+
+        Returns:
+            VoxelData object with the same shape and same metadata as brain_regions. Voxels have
+            uint8 labels (char type) and each voxel label corresponds to the voxel layer index.
+        """
+
+        def _callback():
+            brain_regions = self.load_data('brain_regions')
+            _, layer_ids = self.get_layers()
+            layers = np.zeros_like(brain_regions.raw, dtype=np.uint8)
+            for index, ids in enumerate(layer_ids, 1):
+                mask = math_utils.isin(brain_regions.raw, ids)
+                layers[mask] = index
+
+            return brain_regions.with_data(layers)
+
+        return self._check_cache(
+            ('layer_volume'),
+            callback=_callback,
+            memcache=memcache,
+        )
