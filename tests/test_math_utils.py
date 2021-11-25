[NSETM-1668] Fix rotation around x and z axis in angles_to_matrices

tests/test_math_utils.py
@@ -37,18 +37,18 @@ def test_lcmm():
     npt.assert_equal(12, test_module.lcmm([2, 3, 4]))
 
 
-def test_angles_to_matrices_1():
+def test_angles_to_matrices_x():
     angles = [np.pi / 2]
     expected = [[
         [1, 0, 0],
-        [0, 0, 1],
-        [0, -1, 0],
+        [0, 0, -1],
+        [0, 1, 0],
     ]]
     result = test_module.angles_to_matrices(angles, 'x')
     npt.assert_almost_equal(expected, result)
 
 
-def test_angles_to_matrices_2():
+def test_angles_to_matrices_y():
     angles = [np.pi / 2]
     expected = [[
         [0, 0, 1],
@@ -59,11 +59,11 @@ def test_angles_to_matrices_2():
     npt.assert_almost_equal(expected, result)
 
 
-def test_angles_to_matrices_3():
+def test_angles_to_matrices_z():
     angles = [np.pi / 2]
     expected = [[
-        [0, 1, 0],
-        [-1, 0, 0],
+        [0, -1, 0],
+        [1, 0, 0],
         [0, 0, 1],
     ]]
     result = test_module.angles_to_matrices(angles, 'z')

voxcell/math_utils.py
@@ -4,6 +4,7 @@ import functools
 import math
 
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 
 def gcd(a, b):
@@ -72,22 +73,7 @@ def angles_to_matrices(angles, axis):
     Returns:
         (N, 3, 3) array of rotation matrices.
     """
-    i, j, k = {
-        'x': (1, 2, 0),
-        'y': (0, 2, 1),
-        'z': (0, 1, 2),
-    }[axis]
-
-    cos, sin = np.cos(angles), np.sin(angles)
-
-    result = np.zeros((len(angles), 3, 3))
-    result[:, i, i] = cos
-    result[:, i, j] = sin
-    result[:, j, i] = -sin
-    result[:, j, j] = cos
-    result[:, k, k] = 1.0
-
-    return result
+    return Rotation.from_euler(axis, angles).as_matrix()
 
 
 def normalize(vs):